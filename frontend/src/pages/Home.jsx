f19bccbc7fff7cc17757acdd2da872b39fc65c02
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-05-17 09:33:54 +0530

    Created Offer page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Popular from '../components/Popular';
 import Offer from '../components/Offer';
+import NewCollection from '../components/NewCollection';
+import NewsLetter from '../components/NewsLetter';
 
 const Home =()  => {
   return (
@@ -9,6 +11,8 @@ const Home =()  => {
       <Hero />
       <Popular />
       <Offer />
+      <NewCollection />
+      <NewsLetter />
     </>
   )
 }
