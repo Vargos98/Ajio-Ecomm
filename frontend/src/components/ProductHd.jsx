641f4e2552486db07b74b8869b75b4abe37be453
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-05-17 11:22:41 +0530

    update

---
+++ frontend/src/components/ProductHd.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductHd = () => {
+  return (
+    <div>ProductHd</div>
+  )
+}
+
+export default ProductHd
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import React, { use } from 'react'
+import { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom'
+import ProductHd from '../components/ProductHd'
 const Product = () => {
+  const {all_products} = useContext(ShopContext)
+  const {productId} = useParams();
+  const product = all_products.find((e)=>e.id ===Number(productId));
   return (
-    <div>Product</div>
+    <section>
+      <div>
+        <ProductHd />
+      </div>
+    </section>
   )
 }
 
